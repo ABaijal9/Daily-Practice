c6d11fd97d2e007256a53b4543b9da81b8cc96b4
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-02-22 23:22:08 +0530

    Add files via upload

--- bubble sort with pure recursion.cpp
+++ bubble sort with pure recursion.cpp
@@ -2,46 +2,67 @@
 using namespace std;
 
 void bubblesortpurerecursion(int ar[],int n , int i, int j){
+    //WAY 1: WAY OF MAM
 	//base case
 	if(i == n-1){
-	return;
+		return;
 	}
 
-	if(j==n-1-i){
-	 bubblesortpurerecursion(ar, n , i+1, 0);
-	 cout<< "hey" << i << endl;
+	//recursion case
+	if(j==n-1-i){	
+	 bubblesortpurerecursion(ar, n , i+1, 0);	
 }
 
     else{
-    	if(ar[j]>ar[j+1]){
-	    swap(ar[j],ar[j+1]);
-	    cout<< j << endl;
-	}  
-}
-
-	//recursion case
-	 bubblesortpurerecursion(ar, n , i, j+1);
-}
-
-// void print(int ar[], int n, int i){
-
-// 	if (ar[i] == n);
+    	if(ar[j] > ar[j+1]){
+	    swap(ar[j],ar[j+1]); 
+	}
 	
-// 	if (ar[i] < n)
-// 	cout<< ar[i];
-    
-//     print(ar, n ,i+1);
-// }
+	bubblesortpurerecursion(ar, n , i, j+1);
+}
+}
+
+void printarray(int ar[], int n){
+     
+     for(int i = 0; i < n ; i++){
+        cout<<ar[i];
+        }
+        cout << endl;   
+}
 
 int main(){
-	int n = 5;
-	int ar[n] = {5,4,3,2,1};
-    cout << "n" << n << endlc;out << n << endl; 
+
+	int ar[] = {5,4,3,2,1};
+	int n = sizeof(ar)/sizeof(int);
+
+    printarray(ar,n);
 	bubblesortpurerecursion(ar, n , 0, 0);
-	// print(ar, n, 0);
-	cout << "n" << n << endl; 
-	for(int i = 0; i < n; i++){
-		cout<< ar[i];
-	}
+	printarray(ar,n);
 
 }
+
+// void bubblesortpurerecursion(int ar[],int n , int i, int j){
+
+// 	//WAY 2: CHECK HOW IT IS WORKING
+// 	//base case
+// 	if(i == n-1){
+// 		return;
+// 	}
+
+// 	if(j == n){
+// 		return;
+// 	}
+// 	if(j==n-1-i){
+// 		// cout<< "bey " << i << " " << j << endl;
+// 	 bubblesortpurerecursion(ar, n , i+1, 0);
+// 	 // cout<< "hey " << i << " " << j << endl;
+// }
+//     else{
+//     	if(ar[j] > ar[j+1]){
+// 	    swap(ar[j],ar[j+1]);
+// //      cout<< " here i and j are " << i << " " << j << endl;
+// 	}  
+// }
+// 	//recursion case
+// 	 bubblesortpurerecursion(ar, n , i, j+1);
+// }
